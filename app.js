16ae64f4da2648ea10f8d6e235c90fdb84f59a06
hinst <hinstance@yandex.ru>
2026-02-16 22:05:03 +0100

    Revise parameter names: request, response

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const PRETTIER_BIN = path.join(__dirname, 'node_modules', '.bin', 'prettier');
 const app = express();
 app.use(express.text({ type: '*/*' }));
 
-app.post('/', (req, res) => {
-	const { filename } = req.query;
+app.post('/', (request, response) => {
+	const { filename } = request.query;
 	if (typeof filename !== 'string')
-		return res.status(400).send('Need query parameter: filename of type string');
+		return response.status(400).send('Need query parameter: filename of type string');
 
 	const result = spawnSync(PRETTIER_BIN, ['--stdin-filepath', filename], {
-		input: req.body,
+		input: request.body,
 		shell: true,
 	});
 
 	if (result.status !== 0)
-		res.status(400).send(result.stderr.toString());
+		response.status(400).send(result.stderr.toString());
 	else
-		res.type('text/plain').send(result.stdout.toString());
+		response.type('text/plain').send(result.stdout.toString());
 });
 
 app.listen(3000, () => console.log('Prettier server listening on port 3000'));
