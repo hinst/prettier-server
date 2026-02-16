fb9cd261191ab6176f99d3f6749de23137ef9914
hinst <hinstance@yandex.ru>
2026-02-16 21:31:02 +0100

    Use spawn() sync

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 const path = require('path');
 
 const PRETTIER_BIN = path.join(__dirname, 'node_modules', '.bin', 'prettier');
@@ -12,26 +12,15 @@ app.post('/', (req, res) => {
 	if (typeof filename !== 'string')
 		return res.status(400).send('Need query parameter: filename of type string');
 
-	const prettier = spawn(PRETTIER_BIN, ['--stdin-filepath', filename], {
+	const result = spawnSync(PRETTIER_BIN, ['--stdin-filepath', filename], {
+		input: req.body,
 		shell: true,
 	});
 
-	const out = [];
-	const err = [];
-
-	prettier.stdout.on('data', (d) => out.push(d));
-	prettier.stderr.on('data', (d) => err.push(d));
-
-	prettier.on('close', (code) => {
-		if (code !== 0) {
-			res.status(400).send(Buffer.concat(err).toString());
-		} else {
-			res.type('text/plain').send(Buffer.concat(out).toString());
-		}
-	});
-
-	prettier.stdin.write(req.body);
-	prettier.stdin.end();
+	if (result.status !== 0)
+		res.status(400).send(result.stderr.toString());
+	else
+		res.type('text/plain').send(result.stdout.toString());
 });
 
 app.listen(3000, () => console.log('Prettier server listening on port 3000'));
