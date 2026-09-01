eb08dd1576e6b279a273e8be254d7b3da4d48df1
hinst <hinstance@yandex.ru>
2026-09-01 14:10:09 +0200

    Optimization: use Prettier directly instead of spawning process.
    By Qwen v3.8 27B.

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 //@ts-check
 const express = require('express');
-const { spawnSync } = require('child_process');
-const path = require('path');
+const prettier = require('prettier');
 
-const PRETTIER_BIN = path.join(__dirname, 'node_modules', '.bin', 'prettier');
 const PORT_NUMBER = 3000;
 
 const app = express();
 app.use(express.text({ type: '*/*', limit: '100mb' }));
 
-app.post('/', (request, response) => {
+app.post('/', async (request, response) => {
 	const { filename } = request.query;
 	if (typeof filename !== 'string')
 		return response.status(400).send('Need query parameter: filename of type string');
 
-	const result = spawnSync(PRETTIER_BIN, ['--stdin-filepath', filename], {
-		input: request.body,
-		shell: true,
-	});
-
-	if (result.status !== 0)
-		response.status(400).send(result.stderr.toString());
-	else
-		response.type('text/plain').send(result.stdout.toString());
+	try {
+		const formatted = await prettier.format(request.body, { filepath: filename });
+		response.type('text/plain').send(formatted);
+	} catch (error) {
+		const message = error instanceof Error ? error.message : String(error);
+		response.status(400).send(message);
+	}
 });
 
 app.listen(PORT_NUMBER, () => console.log('Prettier server listening on port ' + PORT_NUMBER));
